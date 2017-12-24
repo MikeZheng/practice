3103c19c0a09536a06620e4eda46e04bcfeff34c
zhengzhenfu <zhengzhenfu@ZC500079PC1.hdsc.com>
2017-12-24 19:08:38 +0800

    add files to learning mybatis deep

--- test-zlx/src/main/java/com/zrich/App.java
+++ test-zlx/src/main/java/com/zrich/App.java
@@ -1,18 +1,44 @@
 package com.zrich;
 
 import java.io.IOException;
-import java.util.Date;
-import java.util.Scanner;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import com.zrich.mapper.UserMapper;
+import com.zrich.model.User;
 
 /**
  * Hello world!
  */
 public class App {
-    public static void main(String[] args) throws IOException {
-        System.out.println("Hello World!");        
+  public static void main(String[] args) throws IOException {
+
+    String path = "mybatis-config.xml";
+    SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(Resources.getResourceAsReader(path));
+
+    try (SqlSession session = factory.openSession()) {
+      UserMapper userMapper = session.getMapper(UserMapper.class);
+      User user = new User();
+      user.setId(3);
+      user.setName("David");
+      user.setAddress("广东省深圳市");
+      user.setEmail("123@123.com");
+      user.setGender("M");
+      userMapper.insert(user);
+      session.commit();
     }
-    
-    
+
+    /*
+     * Connection connection=null; try { Class.forName("org.postgresql.Driver"); connection =
+     * DriverManager.getConnection("jdbc:postgresql://localhost:5432/test"); } catch (Exception e) {
+     * e.printStackTrace(); }
+     */
+
+
+  }
+
+
 }
