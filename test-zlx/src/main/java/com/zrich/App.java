1132983dbe19b859dede8bb5fac8d046ec3dd16f
zhengzhenfu <zhengzhenfu@hdsc.com>
2017-12-27 15:12:07 +0800

    use log4j2

--- test-zlx/src/main/java/com/zrich/App.java
+++ test-zlx/src/main/java/com/zrich/App.java
@@ -6,9 +6,12 @@ import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import com.zrich.mapper.UserMapper;
 import com.zrich.model.User;
+import com.zrich.model.UserExample;
 
 /**
  * Hello world!
@@ -21,13 +24,15 @@ public class App {
 
     try (SqlSession session = factory.openSession()) {
       UserMapper userMapper = session.getMapper(UserMapper.class);
-      User user = new User();
-      user.setId(3);
-      user.setName("David");
-      user.setAddress("广东省深圳市");
-      user.setEmail("123@123.com");
-      user.setGender("M");
-      userMapper.insert(user);
+//      User user = new User();
+//      user.setId(3);
+//      user.setName("David");
+//      user.setAddress("广东省深圳市");
+//      user.setEmail("123@123.com");
+//      user.setGender("M");
+//      userMapper.insert(user);
+      UserExample example = new UserExample();
+      System.out.println(userMapper.countByExample(example ));
       session.commit();
     }
 
@@ -37,8 +42,34 @@ public class App {
      * e.printStackTrace(); }
      */
 
-
+    Logger logger = LogManager.getLogger("mylog");
+    logger.debug("debug");
+    logger.error("error");
+    logger.info("info");
+    logger.trace("trace");
+    logger.warn("warn");
+    logger.fatal("fatal");
+    
+    for(int i = 0; i < 50000; i++) {  
+      logger.trace("trace level");  
+      logger.debug("debug level");  
+      logger.info("info level");  
+      logger.warn("warn level");  
+      logger.error("error level");  
+      logger.fatal("fatal level");  
+  }  
+  try {  
+      Thread.sleep(1000 * 61);  
+  } catch (InterruptedException e) {}  
+  logger.trace("trace level");  
+  logger.debug("debug level");  
+  logger.info("info level");  
+  logger.warn("warn level");  
+  logger.error("error level");  
+  logger.fatal("fatal level");  
+    
   }
+  
 
 
 }
